d8fca0f8aa753341a8f651683792694decd05423
yurak-choi <nozo8026@gmail.com>
2021-04-13 17:16:13 +0900

    css module

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import SassContent from './components/sass/SassContent';
+import ModuleContent from "./components/module/ModuleContent";
 
 function App() {
   return (
     <div className="App">
       <SassContent />
+      <ModuleContent />
     </div>
   );
 }
---
+++ src/components/module/ModuleContent.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import CheckBox from './CheckBox';
+
+function ModuleContent() {
+  const [check, setCheck] = useState(false);
+  const onChange = e => setCheck(e.target.checked);
+
+  return (
+    <div>
+      <CheckBox onChange={onChange} checked={check}>
+        다음 약관에 모두 동의
+      </CheckBox>
+      <p>
+        <b>check: </b>
+        {check ? 'true' : 'false'}
+      </p>
+    </div>
+  );
+}
+
+export default ModuleContent;
