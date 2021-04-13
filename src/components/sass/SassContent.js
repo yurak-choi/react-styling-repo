d14d692d8fd120098eb4aaec1f0bafdc3af898f1
yurak-choi <nozo8026@gmail.com>
2021-04-13 14:27:15 +0900

    sass

---
+++ src/components/sass/SassContent.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Button from "./Button";
+
+function SassContent() {
+  return (
+    <>
+      <div className="buttons">
+        <Button size="large" onClick={() => console.log('클릭됐다!')}>BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="gray">BUTTON</Button>
+        <Button color="gray">BUTTON</Button>
+        <Button size="small" color="gray">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="pink">BUTTON</Button>
+        <Button color="pink">BUTTON</Button>
+        <Button size="small" color="pink">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" color="blue" outline>BUTTON</Button>
+        <Button color="gray" outline>BUTTON</Button>
+        <Button size="small" color="pink" outline>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>BUTTON</Button>
+        <Button size="large" fullWidth color="gray">BUTTON</Button>
+        <Button size="large" fullWidth color="pink">BUTTON</Button>
+      </div>
+    </>
+  );
+}
+
+export default SassContent;
