65cf35f8f90dfa9a459611b9a039741d74af96f8
yurak-choi <nozo8026@gmail.com>
2021-04-15 12:08:02 +0900

    styled component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.scss';
 import SassContent from './components/sass/SassContent';
-import ModuleContent from "./components/module/ModuleContent";
+import ModuleContent from './components/module/ModuleContent';
+import SCContent from './components/sc/SCContent';
 
 function App() {
   return (
     <div className="App">
       <SassContent />
       <ModuleContent />
+      <SCContent />
     </div>
   );
 }
